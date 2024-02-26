d70aa8fb033a60834ffb947219cbd58f8117ca5e
zain00abd <zainabflash1998@gmail.com>
2024-02-26 07:55:00 +0200

    wq

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.set("view engine", "ejs");
 const port = process.env.PORT || 5000
 
 app.get('/', (req,res) =>{
-    res.send("hello zain server method")
+    res.render("index")
 })
 
 mongoose.connect("mongodb+srv://zaindiv:SK7A2fOZbLeJ08Ix@cluster0.32r5dqe.mongodb.net/all-data?retryWrites=true&w=majority")
