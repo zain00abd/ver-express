2c7f40e662bce67561a30e909523a3e94eb39583
zain00abd <zainabflash1998@gmail.com>
2024-02-26 07:03:15 +0200

    method

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 import express from "express"
 import mongoose from "mongoose"
-const methodOverriade = require("method-override")
 
 const app = express()
-app.use(methodOverriade("_method"))
-
-
 const port = process.env.PORT || 5000
 
 app.get('/', (req,res) =>{
-    res.send("hello zain server method")
+    res.send("hello zain server")
 })
 
 mongoose.connect("mongodb+srv://zaindiv:SK7A2fOZbLeJ08Ix@cluster0.32r5dqe.mongodb.net/all-data?retryWrites=true&w=majority")
