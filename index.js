5c1b8e5d0e4b157dd4488dc2fb8f74fc97db0a8f
zain00abd <zainabflash1998@gmail.com>
2024-02-26 06:56:16 +0200

    consoleSS

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.get('/', (req,res) =>{
-    res.send("hello zain1")
+    res.send("hello zain server")
 })
 
 mongoose.connect("mongodb+srv://zaindiv:SK7A2fOZbLeJ08Ix@cluster0.32r5dqe.mongodb.net/all-data?retryWrites=true&w=majority")
