324a8ff6cd375afb728e848ab04b05de33d97926
zain00abd <zainabflash1998@gmail.com>
2024-02-26 07:38:58 +0200

    vv

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import express from "express"
 import mongoose from "mongoose"
 import methodOverriade from "method-override"
-import InvoiceModel from './models/datashcema.js';
-
 
 const app = express()
 
@@ -14,18 +12,9 @@ app.set("view engine", "ejs");
 
 const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-
-    InvoiceModel.find()
-        .then((result) => {
-            res.render("index", { arr: result });
-            console.log(result)
-
-        })
-        .catch((error) => {
-            console.error(error);
-        });
-});
+app.get('/', (req,res) =>{
+    res.send("hello zain server method")
+})
 
 mongoose.connect("mongodb+srv://zaindiv:SK7A2fOZbLeJ08Ix@cluster0.32r5dqe.mongodb.net/all-data?retryWrites=true&w=majority")
 .then(() =>{
