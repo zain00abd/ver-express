26f8dd56a8934e9cfc405ef1f63ba4f912e6e8fa
zain00abd <zainabflash1998@gmail.com>
2024-02-27 22:41:09 +0200

    swe

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
-import methodOverride from "method-override"; // Corrected typo
-import InvoiceModel from './models/datashcema.js';
+// import methodOverride from "method-override"; // Corrected typo
+// import InvoiceModel from './models/datashcema.js';
 
 
 const app = express();
 
 // app.use(methodOverride("_method"));
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
+// app.use(express.static("public"));
+// app.use(express.urlencoded({ extended: true }));
+// app.set("view engine", "ejs");
 
 const port = process.env.PORT || 5000;
-const dbUri = process.env.MONGODB_URI; // Use an environment variable for your MongoDB URI
+
 
 app.get('/', (req, res) => {
     console.log("result sfdfsd");
